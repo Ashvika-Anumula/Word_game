bacd5f786ba1fd047b8a00fe584571a1d795795d
Ashvika-Anumula <107846312+Ashvika-Anumula@users.noreply.github.com>
2022-11-17 19:24:06 +1100

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+function add_user()
+{
+    player1=document.getElementById("player1_name_input").value;
+    player2=document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("name1",player1);
+    localStorage.setItem("name2",player2);
+
+    window.location="game_page.html";
+}
